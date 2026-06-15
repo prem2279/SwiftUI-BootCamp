c16afbed22a889bb79cf7b473399b4a45d0e4c39
Prem Gundu <60621227+prem2279@users.noreply.github.com>
2026-06-15 14:35:50 -0400

    Initial Commit

---
+++ SwiftUI BootCamp/SwiftUI_BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootCampApp.swift
+//  SwiftUI BootCamp
+//
+//  Created by Prem Kumar Gundu on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI BootCampTests/SwiftUI_BootCampTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUI_BootCampTests.swift
+//  SwiftUI BootCampTests
+//
+//  Created by Prem Kumar Gundu on 6/15/26.
+//
+
+import Testing
+@testable import SwiftUI_BootCamp
+
+struct SwiftUI_BootCampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
