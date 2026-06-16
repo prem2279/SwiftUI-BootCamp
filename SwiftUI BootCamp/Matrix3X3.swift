74de180af28b7e9fa9164ec4ba6c8fbed9662d6d
Prem Gundu <60621227+prem2279@users.noreply.github.com>
2026-06-16 10:31:25 -0400

    3x3 Matrix using text labels, VStack, HStack

---
+++ SwiftUI BootCamp/Matrix3X3.swift
@@ -0,0 +1,39 @@
+//
+//  ColorsBootCamp.swift
+//  SwiftUI BootCamp
+//
+//  Created by Prem Kumar Gundu on 6/16/26.
+//
+
+import SwiftUI
+
+struct ColorsBootCamp: View {
+    var body: some View {
+        VStack{
+            HStack{Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)}
+            Spacer()
+            HStack{Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)}
+            Spacer()
+            HStack{Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)}
+        }
+        .padding()
+        
+        
+    }
+}
+
+#Preview {
+    ColorsBootCamp()
+}
